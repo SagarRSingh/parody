3dd9da4dc3205109aace0eed139c1ee6d189df7d
shailesh.chaudhar <kumarchaudhary142@gmail.com>
2022-09-24 11:09:51 +0530

    login Module working

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent, } from './login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDialogModule,
+    
+
+   
+  ],
+  exports:[LoginComponent,]
+})
+export class LoginModule { }
